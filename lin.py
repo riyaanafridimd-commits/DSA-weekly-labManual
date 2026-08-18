9630cd2f59cce1ced997f67c715eb54bdda44474
riyaanafridimd-commits <riyaanafridimd@gmail.com>
2026-08-18 11:14:27 +0530

    Add files via upload

---
+++ lin.py
@@ -0,0 +1,50 @@
+'''def linear(arr, key):
+    for i in range(len(arr)):
+        if arr[i] == key:
+            return i
+    return -1
+def main():
+    n = int(input("get me the size of list:\t"))
+    if n <= 0:
+        print("Try again.....")
+        return
+    arr = []
+    for i in range(n):
+        s = int(input(f"get me the element {i+1}:\t"))
+        arr.append(s)
+    key = int(input("get me the value to search:\t"))
+    if linear(arr, key) != -1:
+        print(f"Element {key} is found in array")
+    else:
+        print("Not Found")
+main()'''
+
+
+
+def linear(arr, key):
+    for i in range(len(arr) + 1):   # intentional error
+        if arr[i] == key:
+            return i
+    return -1
+
+def main():
+    n = int(input("get me the size of list:\t"))
+
+    if n <= 0:
+        print("Try again.....")
+        return
+
+    arr = []
+
+    for i in range(n):
+        s = int(input(f"get me the element {i+1}:\t"))
+        arr.append(s)
+
+    key = int(input("get me the value to search:\t"))
+
+    if linear(arr, key) != -1:
+        print(f"Element {key} is found in array")
+    else:
+        print("Not Found")
+
+main()
